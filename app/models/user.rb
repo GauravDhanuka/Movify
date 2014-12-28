7cbb943d9525ababf3104cc768fea1a77ea4135f
Aruj Parajuli <arujparajuli@gmail.com>
2014-12-29 03:01:39 +0530

    Review

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :movies
-  
+  has_many :movies #Every user have movies
+  has_many :reviews, dependent: :destroy #if user get destroyed then all reviews all get destroyed with it.  
+
 end
